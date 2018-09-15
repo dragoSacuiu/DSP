5e2ae00b5de75f720cda4a9d1c1c04805d62ed0d
Sacuiu Dragos-Alexandru <drs@sacuius-imac.local>
2018-09-15 04:19:48 +0300

    Initial Commit

---
+++ DSP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DSP
+//
+//  Created by Sacuiu Dragos on 15/09/2018.
+//  Copyright © 2018 Sacuiu Dragos. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
